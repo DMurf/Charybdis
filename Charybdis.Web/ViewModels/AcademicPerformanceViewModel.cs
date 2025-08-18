7fc765b4b06094e9998396a09a5e09004dd3125c
Dan Murfitt <Dan.MURFITT@EDUCATION.GOV.UK>
2025-08-18 13:41:19 +0100

    Remove model warnings, remove external js, enforce csp.

--- Charybdis.Web/ViewModels/AcademicPerformanceViewModel.cs
+++ Charybdis.Web/ViewModels/AcademicPerformanceViewModel.cs
@@ -5,12 +5,12 @@ namespace Charybdis.Web.ViewModels
 {
     public class AcademicPerformanceViewModel
     {
-        public EstablishmentCore EstablishmentCore { get; set; }
+        public EstablishmentCore EstablishmentCore { get; set; } = default!;
 
-        public KS4_Grade5EngMath KS4_Grade5EngMath { get; set; }
+        public KS4_Grade5EngMath KS4_Grade5EngMath { get; set; } = default!;
 
-        public KS4_SubjectEntries KS4_SubjectEntries { get; set; }
+        public KS4_SubjectEntries KS4_SubjectEntries { get; set; } = default!;
 
-        public KS4_Destinations KS4_Destinations { get; set; }
+        public KS4_Destinations KS4_Destinations { get; set; } = default!;
     }
 }
